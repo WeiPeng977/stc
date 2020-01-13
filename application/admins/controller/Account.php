382aaa3b646939537f810af77bc36a2605f6ed3c
aPeng977 <1250025168@qq.com>
2020-01-13 17:02:56 +0800

    "register"

--- application/admins/controller/Account.php
+++ application/admins/controller/Account.php
@@ -8,4 +8,53 @@ class Account extends Controller
     {
         return $this->fetch();
     }
+    public function register()
+    {
+        return $this->fetch();
+    }
+    public function account()
+    {
+        return $this->fetch();
+    }
+    public function cart()
+    {
+        return $this->fetch();
+    }
+    public function home()
+    {
+        return $this->fetch();
+    }
+    public function modifyNumber()
+    {
+        return $this->fetch();
+    }
+    public function modifyPass()
+    {
+        return $this->fetch();
+    }
+    public function order_all()
+    {
+        return $this->fetch();
+    }
+    public function order_payment()
+    {
+        return $this->fetch();
+    }
+    public function order_receive()
+    {
+        return $this->fetch();
+    }
+    public function order_send()
+    {
+        return $this->fetch();
+    }
+    public function order()
+    {
+        return $this->fetch();
+    }
+    public function personal_information()
+    {
+        return $this->fetch();
+    }
+    
 }
