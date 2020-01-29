2b8a1efdec8cfbd244d5c15caf6643b510aa1e35
SH0126 <2394058605@qq.com>
2020-01-29 12:25:09 +0800

    login.php

--- application/index/controller/Account.php
+++ application/index/controller/Account.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use Util\data\Sysdb;
 
 class Account extends Controller
 {
@@ -10,8 +11,37 @@ class Account extends Controller
     }
     public function login()
     {
-        return $this->fetch();
+      return $this->fetch();
     }
+    public function dologin(){
+  		 $username = trim(input('post.username'));
+  		 $pwd = trim(input('post.pwd'));
+      // exit(json_encode(array('code'=>1,'msg'=>'test')));
+  		if($username == ''){
+  		exit(json_encode(array('code'=>1,'msg'=>'用户名不能为空')));
+  		}
+  		if($pwd == ''){
+  	  exit(json_encode(array('code'=>1,'msg'=>'密码不能为空')));
+  		}
+  		// 验证用户
+  		$this->db = new Sysdb;
+  		$user = $this->db->table('user')->where(array('username'=>$username))->item();
+  		if(!$user){
+  			exit(json_encode(array('code'=>1,'msg'=>'用户不存在')));
+  		}
+  		if($pwd != $user['password']){
+  			exit(json_encode(array('code'=>1,'msg'=>'密码错误')));
+  		}
+      //exit(json_encode(array('code'=>1,'msg'=>'test')));
+  		// 设置用户session
+  		session('user',$user);
+  		exit(json_encode(array('code'=>0,'msg'=>'登录成功')));
+  	}
+
+  	public function logout(){
+  		session('admin',null);
+  		exit(json_encode(array('code'=>0,'msg'=>'退出成功')));
+  	}
     public function register()
     {
         return $this->fetch();
