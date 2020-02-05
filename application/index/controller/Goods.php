31ff71794a705b89b01d4dd3facc03cd84978a00
aPeng977 <1250025168@qq.com>
2020-02-05 15:53:51 +0800

    商品列表，商品详情

---
+++ application/index/controller/Goods.php
@@ -0,0 +1,37 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+use Util\data\Sysdb;
+
+class Goods extends Controller
+{
+  public function __construct(){
+    parent::__construct();
+    $this->db = new Sysdb;
+  }
+    public function goods()
+    {
+        $data['type_id'] = (int)input('get.type_id');
+        $data['pageSize'] = 4;
+    		$data['page'] = max(1,(int)input('get.page'));
+
+    		$data['wd'] = trim(input('get.wd'));
+    		$where = array();
+    		$data['wd'] && $where = 'title like "%'.$data['wd'].'%"';
+
+        if($data['type_id']){
+            $where['type_id'] = $data['type_id'];
+        }
+
+    		$data['data'] = $this->db->table('goods')->where($where)->order('id desc')->pages($data['pageSize']);
+        $this->assign('data',$data);
+        return $this->fetch();
+    }
+    public function goodsDetails()
+    {
+        $data['goods_id'] = (int)input('get.goods_id');
+        $data['data'] = $this->db->table('goods')->where(array('id'=>$data['goods_id']))->item();
+        $this->assign('data',$data);
+        return $this->fetch();
+    }
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,30 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use Util\data\Sysdb;
 
 class Index extends Controller
 {
+  public function __construct(){
+		parent::__construct();
+		$this->db = new Sysdb;
+	}
     public function index()
     {
-        return $this->fetch();
+
+      $data['type_id'] = (int)input('get.type_id');
+      $data['pageSize'] = 4;
+      $data['page'] = max(1,(int)input('get.page'));
+
+      $data['wd'] = trim(input('get.wd'));
+      $where = array();
+      $data['wd'] && $where = 'title like "%'.$data['wd'].'%"';
+      $data['data'] = $this->db->table('goods')->where($where)->order('id desc')->pages($data['pageSize']);
+      $this->assign('data',$data);
+
+      $type_list = $this->db->table('goods_label')->where(array('flag'=>'type'))->order('ord asc')->pages(16);
+      $this->assign('type_list',$type_list['lists']);
+      $this->assign('data',$data);
+      return $this->fetch();
     }
 }
